4b903864e9f1e0bc46a98eb2494041c27ea15143
Changok Kim <ckim@twitter.com>
2016-01-04 13:07:48 -0800

    rails4 route style and bump up version

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Jasminerice::Engine.routes.draw do
   resources :spec, :controller => 'spec', :only => [:index] do
     get "fixtures/*filename", :action => :fixtures
   end
-  match "fixtures/*filename", :to => "spec#fixtures"
+  get "fixtures/*filename", :to => "spec#fixtures"
 
   root :to => "spec#index"
 end
